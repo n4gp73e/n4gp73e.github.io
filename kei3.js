8189e5ee8a964611d516452a231b59e36d48f5e9
n4gp73e <mem.ad.pj@gmail.com>
2018-12-13 08:18:26 +0900

    ss

--- kei3.js
+++ kei3.js
@@ -1,3 +1,4 @@
+// ナンバーセット
 function Nstart() {
   'use strict';
   var random1 = Math.floor(Math.random() * 100);
@@ -5,4 +6,15 @@ function Nstart() {
   document.getElementById("num-1").innerHTML = random1;
   document.getElementById("num-2").innerHTML = random2;
 };
-// number sey
+// 乱数生成
+// ID指定する
+
+// 回答出力用
+function Nanser() {
+  var anser = parseInt('num-1')+parseInt('num-2');
+  console.log(anser.innerHTML);
+  document.getElementById('anser').innerHTML = anser;
+};
+// 回答出力用、コンソール入力用
+
+
