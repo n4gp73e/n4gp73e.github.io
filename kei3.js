7c83edeca8b2e777fdadb321b229751c1e0380fb
n4gp73e <mem.ad.pj@gmail.com>
2018-12-13 06:58:45 +0900

    hhhh

---
+++ kei3.js
@@ -0,0 +1,8 @@
+function Nstart() {
+  'use strict';
+  var random1 = Math.floor(Math.random() * 100);
+  var random2 = Math.floor(Math.random() * 100);
+  document.getElementById("num-1").innerHTML = random1;
+  document.getElementById("num-2").innerHTML = random2;
+};
+// number sey
---
+++ soroban.js
@@ -0,0 +1,6 @@
+(function(){
+  'use strict';
+  var random = Math.floor(Math.random() * 100)+1;
+  document.write(random);
+})();
+
